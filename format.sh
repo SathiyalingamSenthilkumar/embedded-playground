51487e01bad5cf0b99d21e504d43527d26ea518d
Sathiya Lingam Senthil Kumar <sathiyalingam.senthilkumar@lumenradio.com>
2025-04-18 19:03:26 +0200

    Move source code into src directory
    The format script is updated to format only files in the src directory.

--- format.sh
+++ format.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-python -m venv venv
+python3 -m venv venv
 venv/bin/pip install -r requirements.txt
 
-find . -not -path './venv/*' -type f \( -iname '*.c' -o -iname '*.h' \) -print0 \
+find src -not -path './venv/*' -type f \( -iname '*.c' -o -iname '*.h' \) -print0 \
     | xargs -0 venv/bin/clang-format -i
